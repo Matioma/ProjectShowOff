538e1604c0db643681ff50a73ec5913e676fa78c
Matioma <Artiom.Matievschi@gmail.com>
2021-05-10 10:23:20 +0200

    Add indicator

--- ProjectShowOff/Assets/Scripts/RPGPlayerController.cs
+++ ProjectShowOff/Assets/Scripts/RPGPlayerController.cs
@@ -8,31 +8,61 @@ public class RPGPlayerController : MonoBehaviour
 {
     [SerializeField]
     Camera cam;
-
     
     NavMeshAgent navMeshAgent;
+    bool isCasting = false;
+
+
+    IndicatorView indicatorView;
 
     public void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         navMeshAgent.updateRotation = false;
+
+        indicatorView = FindObjectOfType<IndicatorView>();
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
+            if (isCasting) {
+                CastSpell();
+                return;
+            }
+
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit)) {
-                // navMeshAgent.velocity = new Vector3();
                 navMeshAgent.SetDestination(hit.point);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            StartCasting();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (isCasting)
+            {
+                StopCasting();
             }
         }
     }
 
-    void LateUpdate()
-    {
-        // transform.rotation = Quaternion.LookRotation(navMeshAgent.velocity.normalized);
+    void StartCasting() {
+        isCasting = true;
+        indicatorView.Show();
+    }
+
+    void StopCasting() {
+        isCasting = false;
+        indicatorView.Hide();
+    }
+
+    void CastSpell() {
+        Debug.Log("Skill Used");
+        StopCasting();
     }
 }
