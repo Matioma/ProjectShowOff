6b73134085735ca9f7b5d2b3c866c448bb0d93dc
Vladislaviii <vladi99sladko@gmail.com>
2021-05-18 12:33:36 +0200

    Working on LevelDemo

--- ProjectShowOff/Assets/Scripts/DangerousObstacle.cs
+++ ProjectShowOff/Assets/Scripts/DangerousObstacle.cs
@@ -28,6 +28,7 @@ public class DangerousObstacle : MonoBehaviour
         CharachterModel charachterModel = collision.gameObject.GetComponent<CharachterModel>();
         if (charachterModel != null) {
             onCollisionWithCharacter?.Invoke();
+            Debug.Log(playerModel.gameObject.name);
             playerModel?.Die();
         }
     }
