5b63ba4f452f6e62456996c419b5e63a3c6914ae
Matioma <Artiom.Matievschi@gmail.com>
2021-06-08 15:26:11 +0200

    Add posibility to compine 2 perlin noise scales

--- ProjectShowOff/Assets/Scripts/PreceduralTools/TerrainBuilder.cs
+++ ProjectShowOff/Assets/Scripts/PreceduralTools/TerrainBuilder.cs
@@ -12,34 +12,66 @@ public class TerrainBuilder : MonoBehaviour, IProcedural
     [SerializeField]
     int height = 30;
 
-    [SerializeField]
-    float scale = 20;
 
+
+
+    
     [SerializeField]
     float offsetX = 100f;
     [SerializeField]
     float offsetZ = 100f;
 
+    [Header("perlin noise 1")]
+    [SerializeField]
+    float scale = 20;
+
+
+
+    [Header("perlin noise 2")]
+    [SerializeField]
+    float scale2 = 20;
+
+
+
     TerrainData getTerrainData(TerrainData terrainData) {
         terrainData.heightmapResolution = width + 1;
         terrainData.size = new Vector3(width, height, depth);
 
-        terrainData.SetHeights(0, 0, generateHeights());
+
+
+        terrainData.SetHeights(0, 0, generateHeights(generateHeights()));
         return terrainData;
     }
+
+
+    float[,] generateHeights(float[,] heights) {
+
+        float[,] newHeights = heights;
+        if (scale2 == 0) return newHeights;
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < depth; z++)
+            {
+                newHeights[x, z] -= CalculateHeight(x, z, scale2);
+            }
+        }
+        return newHeights;
+    }
+
+
 
     float[,] generateHeights() {
         float[,] heights = new float[width, depth];
         for (int x = 0; x < width; x++) {
             for (int z = 0; z < depth; z++) {
-                heights[x, z] = CalculateHeight(x, z);
+                heights[x, z] = CalculateHeight(x, z, scale);
              }
         }
 
         return heights;
     }
 
-    float CalculateHeight(int x , int z) {
+    float CalculateHeight(int x , int z,float scale) {
         float xCoord = (float)x / width * scale + offsetX;
         float zCoord = (float)z / depth * scale + offsetZ;
 
