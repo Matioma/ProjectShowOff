25e3819c769670fc1591360fe7ad3040aa6a6eb0
Matioma <Artiom.Matievschi@gmail.com>
2021-06-16 14:39:48 +0200

    ProgressBar Prefab created

---
+++ ProjectShowOff/Assets/Scripts/Views/ProgressBar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0f,1f)]
+    float progress;
+
+
+    public float Progress {
+        get { return progress; }
+        set {
+            if (value == progress) return;
+
+            progress = value;
+            if (progress > 1) progress = 1;
+            if (progress < 0) progress = 0;
+            updateProgress();
+        }
+    }
+
+    [SerializeField]
+    Image ProgressMaskImage;
+
+    void updateProgress()
+    {
+        ProgressMaskImage.fillAmount = progress;
+    }
+    
+}
