f9c9c20ee01dca830194c166ca12b39678536183
Matioma <Artiom.Matievschi@gmail.com>
2021-06-23 09:45:11 +0200

    Display the right score image

--- ProjectShowOff/Assets/Scripts/Views/ResolutionScreenView.cs
+++ ProjectShowOff/Assets/Scripts/Views/ResolutionScreenView.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+
+
+[System.Serializable]
+public class ImageScore {
+    [SerializeField]
+    public GameObject scoreImage;
+
+    [SerializeField]
+    public int score;
+
+
+    public void show() {
+        scoreImage.SetActive(true);
+    }
+
+    public void hide()
+    {
+        scoreImage.SetActive(false);
+    }
+}
 public class ResolutionScreenView : MonoBehaviour
 {
     private PlayerData data;
@@ -11,22 +31,44 @@ public class ResolutionScreenView : MonoBehaviour
 
     [SerializeField]
     private GameObject NameList;
-
     [SerializeField]
     private GameObject NameTemplate;
+
 
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
 
+    [SerializeField]
+    private ImageScore[] ScoreImages;
+
     private void Awake()
     {
         data = PlayerData.Load(Application.dataPath + Player.FilePath);
+        Debug.Log(data.trashCollectected);
         scoreBoard = ScoreBoard.LoadScore(Application.dataPath + ScoreBoard.filePath);
         scoreBoard.AddScore(data);
         scoreBoard.SaveScore(Application.dataPath + ScoreBoard.filePath);
+        ShowRightImage();
 
     }
+
+    public void ShowRightImage()
+    {
+        //Debug.LogErro
+        for (int i = 0; i < ScoreImages.Length; i++) {
+            ScoreImages[i].hide();
+        }
+        for (int i = ScoreImages.Length - 1; i >= 0; i--)
+        {
+            if (ScoreImages[i].score <= data.trashCollectected) {
+                ScoreImages[i].show();
+                break;
+            }
+        }
+    }
+
+
     private void Start()
     {
         scoreBoard.players.Reverse();
